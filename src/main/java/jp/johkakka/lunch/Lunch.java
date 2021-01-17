7a3701fa3e4c08db6491adab2cb71dd159a4d2f1
johkakka <johkakka.rits@gmail.com>
2021-01-18 00:14:06 +0900

    [update]受け渡しができるように

--- src/main/java/jp/johkakka/lunch/Lunch.java
+++ src/main/java/jp/johkakka/lunch/Lunch.java
@@ -1,24 +1,27 @@
 package jp.johkakka.lunch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Lunch {
 
-    @GetMapping("/")
+    @GetMapping(value={"/", "/index"})
     public String index(){
         return "index";
     }
 
-    @GetMapping("/here")
+    @GetMapping("/roulett")
     public String here(){
         return "roulett";
     }
 
-    @PostMapping
-    public String from(){
+    @PostMapping("/roulett")
+    public String from(ModelMap modelMap, @RequestParam("from") String name){
+        modelMap.addAttribute("from", name);
         return "roulett";
     }
 }
