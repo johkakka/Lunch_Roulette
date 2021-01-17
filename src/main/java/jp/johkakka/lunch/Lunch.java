e9dca8ad0ce2ca0cfebf6668e5498114d9946d09
johkakka <johkakka.rits@gmail.com>
2021-01-18 00:24:09 +0900

    [update]空文字に対応

--- src/main/java/jp/johkakka/lunch/Lunch.java
+++ src/main/java/jp/johkakka/lunch/Lunch.java
@@ -21,6 +21,9 @@ public class Lunch {
 
     @PostMapping("/roulett")
     public String from(ModelMap modelMap, @RequestParam("from") String name){
+        if (name.isBlank()){
+            return "index";
+        }
         modelMap.addAttribute("from", name);
         return "roulett";
     }
