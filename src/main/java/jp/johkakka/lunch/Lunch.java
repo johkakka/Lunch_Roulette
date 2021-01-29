350694506087fa5f99f3e3879655f8de6a6c18a5
Joh_Kakka <47859532+johkakka@users.noreply.github.com>
2021-01-29 16:57:33 +0900

    [add] 結果表示ページを作成 (#1)
    * add internal to gitignore
    
    * s/pos/from
    
    * [update]methodをpostに変更
    
    * [update]タイトル変更
    
    * [fix]見た目を修正
    
    * [update]受け渡しができるように
    
    * [update]空文字に対応
    
    * [update] タイトルらしきものを作成

--- src/main/java/jp/johkakka/lunch/Lunch.java
+++ src/main/java/jp/johkakka/lunch/Lunch.java
@@ -1,24 +1,30 @@
 package jp.johkakka.lunch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Lunch {
 
-    @GetMapping("/")
+    @GetMapping(value={"/", "/index"})
     public String index(){
         return "index";
     }
 
-    @GetMapping("/here")
+    @GetMapping("/roulett")
     public String here(){
-        return "index";
+        return "roulett";
     }
 
-    @PostMapping
-    public String from(){
-        return "index";
+    @PostMapping("/roulett")
+    public String from(ModelMap modelMap, @RequestParam("from") String name){
+        if (name.isBlank()){
+            return "index";
+        }
+        modelMap.addAttribute("from", name);
+        return "roulett";
     }
 }
