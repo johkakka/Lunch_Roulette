1c33577fd01cc52550e56e170d6985654962d231
johkakka <johkakka.rits@gmail.com>
2021-01-29 18:27:16 +0900

    [add] エラー出力ページ作成

--- src/main/java/jp/johkakka/lunch/Lunch.java
+++ src/main/java/jp/johkakka/lunch/Lunch.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class Lunch {
 
@@ -24,6 +32,18 @@ public class Lunch {
         if (name.isBlank()){
             return "index";
         }
+
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.lines(Paths.get("/internal"), StandardCharsets.UTF_8).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            String m = e.toString();
+            modelMap.addAttribute("message", m);
+            return "error";
+        }
+
+
         modelMap.addAttribute("from", name);
         return "roulett";
     }
