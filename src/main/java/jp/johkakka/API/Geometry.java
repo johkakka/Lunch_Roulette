9dbdeef94e2e78805f94d2b31add59a6e70274fb
johkakka <johkakka.rits@gmail.com>
2021-02-01 02:29:21 +0900

    [add] 座標を管理するクラスを作成

--- src/main/java/jp/johkakka/API/Geometry.java
+++ src/main/java/jp/johkakka/API/Geometry.java
@@ -2,7 +2,18 @@ package jp.johkakka.API;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties (ignoreUnknown = true)
 public class Geometry {
-    
+    private Location location;
+    private List<String> types;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
 }
