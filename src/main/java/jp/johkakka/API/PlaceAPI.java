a5cf8fdd548855d006a7fcd328be6cb4d618e046
johkakka <johkakka.rits@gmail.com>
2021-02-02 21:37:49 +0900

    [add] PlaceAPIの雛形を作成

--- src/main/java/jp/johkakka/API/PlaceAPI.java
+++ src/main/java/jp/johkakka/API/PlaceAPI.java
@@ -4,7 +4,15 @@ import jp.johkakka.JSON.Place;
 
 public class PlaceAPI extends GoogleAPI{
     public Place result(String loc){
-        return null;
+        Place res = null;
+
+        String key = super.getKey();
+        if (key == null){
+            return res;
+        }
+        super.setPath("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+loc+"&radius=1000&type=restaurant&language=ja&key="+key);
+
+        return res;
     }
 
 }
