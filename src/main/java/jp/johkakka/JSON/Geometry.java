bf1fd34fbcd9c8843dd6d7a1f2255db85b6ae534
johkakka <johkakka.rits@gmail.com>
2021-05-03 19:55:44 +0900

    [update] 使われていないコードを削除

--- src/main/java/jp/johkakka/JSON/Geometry.java
+++ src/main/java/jp/johkakka/JSON/Geometry.java
@@ -2,18 +2,11 @@ package jp.johkakka.JSON;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
-
 @JsonIgnoreProperties (ignoreUnknown = true)
 public class Geometry {
     private Location location;
-    //private List<String> types;
 
     public Location getLocation() {
         return location;
     }
-
-//    public List<String> getTypes() {
-//        return types;
-//    }
 }
