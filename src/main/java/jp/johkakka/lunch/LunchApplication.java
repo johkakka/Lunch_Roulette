668578315e4e1e879acab795ddf7b15d6f102b13
johkakka <johkakka.rits@gmail.com>
2021-08-18 23:30:33 +0900

    [update] SpringBootServletInitializerを継承

--- src/main/java/jp/johkakka/lunch/LunchApplication.java
+++ src/main/java/jp/johkakka/lunch/LunchApplication.java
@@ -2,12 +2,20 @@ package jp.johkakka.lunch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class LunchApplication {
+@RestController
+public class LunchApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LunchApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringApplication.class);
+    }
 }
